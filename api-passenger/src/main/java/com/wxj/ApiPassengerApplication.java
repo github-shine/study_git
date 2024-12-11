e3d9b39e7226ba093123d81235e32af024d5530b
王星君 <2241669945@qq.com>
2024-12-11 20:40:51 +0800

    将代码提交到本地库中

---
+++ api-passenger/src/main/java/com/wxj/ApiPassengerApplication.java
@@ -0,0 +1,13 @@
+package com.wxj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiPassengerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiPassengerApplication.class, args);
+    }
+
+}
---
+++ api-passenger/src/test/java/com/wxj/ApiPassengerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wxj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiPassengerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
